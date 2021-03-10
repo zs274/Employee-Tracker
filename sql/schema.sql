bc31ec56aafb8bfb873024e17152c02ca9c6e998
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-10 01:32:52 +0000

    created tables

---
+++ sql/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employyes_db;
+
+CREATE DATABASE employees_db;
+
+USE employess_db;
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(6,2) NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY(id)
+);
