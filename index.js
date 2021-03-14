8dec77d720f70a213e755dbad4278b66797164e5
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-14 14:58:29 +0000

    created switch cases

--- index.js
+++ index.js
@@ -10,7 +10,56 @@ const connection = mysql2.createConnection({
     database: 'employees_db',
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err
     init();
 });
+
+function init() {
+    prompt();
+}
+
+const prompt = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select what you would like to do",
+            name: "choice",
+            choices: [
+                'View all employees',
+                'View employees by role',
+                'View employees by department',
+                'Update employee',
+                'Add employee',
+                'Add role',
+                'Add department'
+            ]
+        }
+    ]).then(function (value) {
+        switch (value.choice) {
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'View employees by role':
+                viewByRole();
+                break;
+            case 'View employees by department':
+                viewByDepartment();
+                break;
+            case 'Update employee':
+                updateEmployee();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Add department':
+                addDepartment();
+                break;
+
+        }
+
+    })
+}
