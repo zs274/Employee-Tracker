12fe6bac3fd8ad7aa080cae1cf084695290c6b83
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-14 18:49:34 +0000

    created view all employees function

--- index.js
+++ index.js
@@ -59,13 +59,18 @@ const prompt = () => {
                 addDepartment();
                 break;
         }
-
-    })
-}
-
-function viewAllEmployees() {
-
+    });
 };
+
+
+const viewAllEmployees = () => {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+      function (err, res) {
+        if (err) throw err
+        console.table(res)
+        prompt()
+      })
+  };
 
 function viewByRole() {
 
